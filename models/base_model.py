5bc6884fa010c94f17105cb130af522c43bfa4f6
felixokolo <pheelix.info@gmail.com>
2022-08-03 14:05:15 +0100

    addition

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ from datetime import datetime
 class BaseModel:
     """Base model class
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Base class init
         Attributes:
             id (str): unique id assigned when instance is created
@@ -15,9 +15,18 @@ class BaseModel:
             when an instance is created and it will be updated every
             time you change your object
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if (kwargs == {}):
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for k, v in kwargs.items():
+                if (k == '__class__'):
+                    continue
+                if (k == 'updated_at' or k == 'created_at'):
+                    v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+                else:
+                    exec("self." + k + " = v")
 
     #@property
     def __str__(self):
@@ -35,6 +44,8 @@ class BaseModel:
         of the instance
         """
         ret = self.__dict__
+        ret['created_at'] = self.created_at.isoformat()
+        ret['updated_at'] = self.updated_at.isoformat()
         ret['__class__'] = type(self).__name__
         return ret
 
