7df5e3763e41a7b8123e90faf51af9fa4a751644
The-Astronautt <73796349+The-Astronautt@users.noreply.github.com>
2022-08-08 01:54:38 +0100

    Update

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,27 @@
+from multiprocessing.sharedctypes import Value
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
 
-    def __init__(self, name="", my_number=0):
+    def __init__(self, *args, **kwargs,):
         # Initializes the class
-        self.my_number = my_number
-        self.name = name
-        self.updated_at = datetime.now()
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+        # self.my_number = my_number
+        # self.name = name
+        if kwargs != {}:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                self.created_at = datetime.strptime(kwargs['created_at'],
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
+                self.updated_at = datetime.strptime(kwargs['updated_at'],
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
+                self.id = kwargs['id']
+        else:
+            self.updated_at = datetime.now()
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
 
     def __str__(self):
         # A representation of what the class should print out
